44065a1a96a03b38e70f8d7092012bbcae2aaa9c
SeungYn <kwls0407@gmail.com>
2023-06-13 02:43:24 +0900

    feat: user 스키마 작성

---
+++ .history/sanity-studio/schemas/user_20230613023807.js
@@ -0,0 +1,39 @@
+export default {
+  title: 'User',
+  name: 'user', //model name
+  type: 'document',
+  fields: [
+    {
+      title: 'Username', //title is name to see at studio ui
+      name: 'username', // name is database key for accessing
+      type: 'string',
+    },
+    {
+      title: 'Name',
+      name: 'name',
+      type: 'string',
+    },
+    {
+      title: 'Email',
+      name: 'email',
+      type: 'string',
+    },
+    {
+      title: 'Image',
+      name: 'email',
+      type: 'string',
+    },
+    {
+      title: 'Following',
+      name: 'following',
+      type: 'array',
+      of: [
+        {
+          type: 'reference',
+          to: [{type: 'user'}],
+        },
+      ],
+      validation: (Rule) => Rule.unique(),
+    },
+  ],
+}
