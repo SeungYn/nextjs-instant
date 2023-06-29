ff90dbf0571b277a376691d56c12f5275717730d
SeungYn <kwls0407@gmail.com>
2023-06-29 18:34:03 +0900

    refact: 유저 검색 라우터 간소화

---
+++ .history/src/app/api/search/[keyword]/route_20230629183305.ts
@@ -0,0 +1,9 @@
+import { NextRequest } from 'next/server';
+
+type Context = {
+  params: { keyword: string };
+};
+export async function GET(request: NextRequest, context: Context) {
+  return searchUsers(context.params.keyword) //
+    .then((data) => NextResponse.json(data));
+}
